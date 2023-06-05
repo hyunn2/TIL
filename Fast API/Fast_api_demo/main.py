1550d29ea8f0615d00d2c63fd17e6206b396e14e
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-05 16:55:09 +0900

    feat(main.py,-__pycache__): 순차적 동작
    순차적으로 경로를 탐색하기 때문에 함수의 순서가 중요하다는 것 추가

--- Fast API/Fast_api_demo/main.py
+++ Fast API/Fast_api_demo/main.py
@@ -21,3 +21,21 @@ def read_item(item_id: int,
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name" : item.name, "item_id": item_id}
+
+# 순차적으로 확인하고 동작하기 때문에 /user/me를 먼저 선언해야함!
+# 반대로 선언하게 된다면 매개변수 user_id의 값을 "me"로 하여 연결할 수 있기 때문
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "The current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+# @app.get("/users/{gender}")
+# def get_user(gender: str):
+#     return "Type is String"
+
+# @app.get("/users/{num}")
+# def get_user(num: int):
+#     return "Type is int"
