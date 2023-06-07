d798e65b23bf3b4d996af51fd4948e4f16e0a7c8
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-07 10:26:26 +0900

    feat(domain,-main.py): router 생성
    라우터 생성

--- Fast API/Fast_api_demo/main.py
+++ Fast API/Fast_api_demo/main.py
@@ -2,6 +2,8 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from domain.question import question_router
+
 app = FastAPI()
 
 class Item(BaseModel):
@@ -9,33 +11,36 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
-@app.get("/")
-def read_root():
-    return {"Hello" : "World"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello" : "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int,
-              q: Union[str, None] = None):
-    return {"item_id" : item_id, "q": q}
+app.include_router(question_router.router)
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name" : item.name, "item_id": item_id}
 
-# 순차적으로 확인하고 동작하기 때문에 /user/me를 먼저 선언해야함!
-# 반대로 선언하게 된다면 매개변수 user_id의 값을 "me"로 하여 연결할 수 있기 때문
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "The current user"}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int,
+#               q: Union[str, None] = None):
+#     return {"item_id" : item_id, "q": q}
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name" : item.name, "item_id": item_id}
 
-# @app.get("/users/{gender}")
-# def get_user(gender: str):
-#     return "Type is String"
+# # 순차적으로 확인하고 동작하기 때문에 /user/me를 먼저 선언해야함!
+# # 반대로 선언하게 된다면 매개변수 user_id의 값을 "me"로 하여 연결할 수 있기 때문
+# @app.get("/users/me")
+# async def read_user_me():
+#     return {"user_id": "The current user"}
 
-# @app.get("/users/{num}")
-# def get_user(num: int):
-#     return "Type is int"
+# @app.get("/users/{user_id}")
+# async def read_user(user_id: str):
+#     return {"user_id": user_id}
+
+# # @app.get("/users/{gender}")
+# # def get_user(gender: str):
+# #     return "Type is String"
+
+# # @app.get("/users/{num}")
+# # def get_user(num: int):
+# #     return "Type is int"
