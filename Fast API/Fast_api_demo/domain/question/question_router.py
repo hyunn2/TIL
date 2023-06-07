558b57760ffee8b009b3cb6673b4685a6e028e7b
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-07 13:02:24 +0900

    feat(question_schema.py,-question_router.py): 스키마 설정
    Pydantic을 적용한 스키마 설정

--- Fast API/Fast_api_demo/domain/question/question_router.py
+++ Fast API/Fast_api_demo/domain/question/question_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from database import get_db
+from domain.question import question_schema
 from models import Question
 
 router = APIRouter(
@@ -9,7 +10,7 @@ router = APIRouter(
 )
 
 
-@router.get("/list")
+@router.get("/list", response_model=list[question_schema.Question])
 # db 객체가 Session 타입이며 db객체에 get_db에 의해 생성된 객체가 주입됨
 def question_list(db: Session = Depends(get_db)):
     _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
