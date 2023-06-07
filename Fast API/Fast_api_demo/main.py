00898c0d49e167a4ac3e698f6e491a7c80b9279c
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-07 13:47:07 +0900

    feat(main.py): Enum 사용
    Fast API docs

--- Fast API/Fast_api_demo/main.py
+++ Fast API/Fast_api_demo/main.py
@@ -1,10 +1,18 @@
 from typing import Union
+
+from enum import Enum
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 from domain.question import question_router
 
 app = FastAPI()
+
+# Enum 사용
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 class Item(BaseModel):
     name: str
@@ -18,14 +26,26 @@ class Item(BaseModel):
 app.include_router(question_router.router)
 
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int,
-#               q: Union[str, None] = None):
-#     return {"item_id" : item_id, "q": q}
+@app.get("/items/{item_id}")
+def read_item(item_id: int,
+              q: Union[str, None] = None):
+    return {"item_id" : item_id, "q": q}
 
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#     return {"item_name" : item.name, "item_id": item_id}
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name" : item.name, "item_id": item_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    # 비교
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    # 값 확인
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
+
 
 # # 순차적으로 확인하고 동작하기 때문에 /user/me를 먼저 선언해야함!
 # # 반대로 선언하게 된다면 매개변수 user_id의 값을 "me"로 하여 연결할 수 있기 때문
