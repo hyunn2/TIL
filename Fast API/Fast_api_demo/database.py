99c039240868b3f08db5e67bbdbee04cdf29e100
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-07 11:29:46 +0900

    refactor(database.py-question_router.py): 의존성 주입 방식 변경
    with문 -> depends

--- Fast API/Fast_api_demo/database.py
+++ Fast API/Fast_api_demo/database.py
@@ -1,5 +1,3 @@
-import contextlib
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -23,7 +21,6 @@ Base = declarative_base()
 
 
 # DB 세션의 생성과 반환을 자동화하는 제너레이터 함수
-@contextlib.contextmanager
 def get_db():
     db = SessionLocal()
     try:
--- Fast API/Fast_api_demo/domain/question/question_router.py
+++ Fast API/Fast_api_demo/domain/question/question_router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
 
 from database import get_db
 from models import Question
@@ -9,8 +10,6 @@ router = APIRouter(
 
 
 @router.get("/list")
-def question_list():
-    # @contextlib.contextmanager 어노테이션 적용으로 인해 with 사용 가능
-    with get_db() as db:
-        _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
+def question_list(db: Session = Depends(get_db)):
+    _question_list = db.query(Question).order_by(Question.create_date.desc()).all()
     return _question_list
