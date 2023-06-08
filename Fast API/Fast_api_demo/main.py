8a190c3bc74f9a0f520bddb48f3cc987e3f31a05
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-08 10:43:57 +0900

    feat(domain/answer): 답변 등록 기능 추가

--- Fast API/Fast_api_demo/main.py
+++ Fast API/Fast_api_demo/main.py
@@ -8,19 +8,20 @@ from typing import Annotated
 
 from domain.question import question_router
 from domain.user import user_router
+from domain.answer import answer_router
 
 app = FastAPI()
 
 # Enum 사용
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
+# class ModelName(str, Enum):
+#     alexnet = "alexnet"
+#     resnet = "resnet"
+#     lenet = "lenet"
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#     is_offer: Union[bool, None] = None
 
 # @app.get("/")
 # def read_root():
@@ -28,33 +29,34 @@ class Item(BaseModel):
 
 app.include_router(question_router.router)
 app.include_router(user_router.router)
+app.include_router(answer_router.router)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int,
-              q: Union[str, None] = None, short: bool = False):
-    item = {"item_id": item_id, "short":short}
-    return {"item_id" : item_id, "q": q, "sort":short}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int,
+#               q: Union[str, None] = None, short: bool = False):
+#     item = {"item_id": item_id, "short":short}
+#     return {"item_id" : item_id, "q": q, "sort":short}
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name" : item.name, "item_id": item_id}
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name" : item.name, "item_id": item_id}
 
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    # 비교
-    if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    # 값 확인
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
+# @app.get("/models/{model_name}")
+# async def get_model(model_name: ModelName):
+#     # 비교
+#     if model_name is ModelName.alexnet:
+#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
+#     # 값 확인
+#     if model_name.value == "lenet":
+#         return {"model_name": model_name, "message": "LeCNN all the images"}
     
-    return {"model_name": model_name, "message": "Have some residuals"}
+#     return {"model_name": model_name, "message": "Have some residuals"}
 
-@app.get("/items/")
-async def read_tem(q: str | None = Query(max_length=4)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    return results
+# @app.get("/items/")
+# async def read_tem(q: str | None = Query(max_length=4)):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     return results
 
 
 # # 순차적으로 확인하고 동작하기 때문에 /user/me를 먼저 선언해야함!
