75fae16349a2a9aab1aee5c3fe41cc3617600b64
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-08 15:31:26 +0900

    fix(DB): SQLite 오류 해결
    SQLitedptj ORM 사용시 나오는 문제점 해결

--- Fast API/Fast_api_demo/database.py
+++ Fast API/Fast_api_demo/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -17,7 +17,15 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # DB 모델 구성시 필요한 클래스
 Base = declarative_base()
 
-
+# ORM 사용을 위한 DB 수정(SQLite만 추가해줘야함)
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+Base.metadata = MetaData(naming_convention=naming_convention)
 
 
 # DB 세션의 생성과 반환을 자동화하는 제너레이터 함수
