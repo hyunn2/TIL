aefe43b0ccd5ee806053286ab8aee6316055f704
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-08 10:04:24 +0900

    feat(domain/question): 질문 상세 기능 추가

--- Fast API/Fast_api_demo/domain/question/question_crud.py
+++ Fast API/Fast_api_demo/domain/question/question_crud.py
@@ -4,3 +4,7 @@ from sqlalchemy.orm import Session
 def get_question_list(db: Session):
     question_list = db.query(Question).order_by(Question.create_date.desc()).all()
     return question_list
+
+def get_question(db: Session, questio_id: int):
+    question = db.query(Question).get(questio_id)
+    return question
