5f070748a766ee9d8b3374e210124ef48e5b938b
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-15 15:21:10 +0900

    feat(front): API 호출 라이브러리

--- Fast API/Fast_api_demo/frontend/src/App.svelte
+++ Fast API/Fast_api_demo/frontend/src/App.svelte
@@ -1,31 +1,10 @@
 <script>
-  // let message;
+  import Router from 'svelte-spa-router'
+  import Home from "./routes/Home.svelte"
 
-  // fetch("http://127.0.0.1:8000/").then((response) => {
-  //   response.json().then((json) => {
-  //     message = json.msg;
-  //   });
-  // });
-
-  let question_list = []
-
-  function get_question_list() {
-    fetch("http://localhost:8000/api/question/list").then((res) => {
-      res.json().then((json) => {
-        question_list = json
-      })
-    })
+  const routes = {
+    '/': Home,
   }
-
-  get_question_list()
 </script>
 
-<!-- <h1>{message}</h1> -->
-
-<ul>
-  {#each question_list as question}
-    <li>
-      {question.subject}
-    </li>
-  {/each}
-</ul>
+<Router {routes}/>
