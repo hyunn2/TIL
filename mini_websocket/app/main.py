e7755b6c5c956c5ad9027a5c87898b170db9c2f3
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-16 16:58:30 +0900

    feat(mini_websocket): websocket으로 채팅 구현

---
+++ mini_websocket/app/main.py
@@ -0,0 +1,55 @@
+# from starlette.websockets import WebSocket
+
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var ws = new WebSocket("ws://localhost:8000/ws");
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+# websocket 생성
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    # 소켓 연결
+    await websocket.accept()
+    while True:
+        # 데이터 받고 보내기
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
+    
