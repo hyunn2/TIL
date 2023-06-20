c651c073863dd04f61168ddff058780213c04e7f
김나현 <nahkim@nahkimui-MacBookPro.local>
2023-06-20 10:36:16 +0900

    feat(main.py,-front): Websocket 채팅 기능 추가

--- Fast API/Fast_api_demo/app/main.py
+++ Fast API/Fast_api_demo/app/main.py
@@ -1,19 +1,14 @@
-from typing import Union
+import asyncio
 
-from enum import Enum
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, WebSocket, status
 from starlette.middleware.cors import CORSMiddleware
+from starlette.websockets import WebSocketDisconnect
 
-from pydantic import BaseModel
-from typing import Annotated
+from datetime import datetime
 
 from domain.question import question_router
 from domain.user import user_router
 from domain.answer import answer_router
-
-# import models
-# from database import engine
-# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -31,67 +26,46 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Enum 사용
-# class ModelName(str, Enum):
-#     alexnet = "alexnet"
-#     resnet = "resnet"
-#     lenet = "lenet"
+class ConnectionManager:
+    def __init__(self):
+        self.activate_connections: list[WebSocket] = []
+    
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.activate_connections.append(websocket)
+    
+    def disconnect(self, websocket: WebSocket):
+        self.activate_connections.remove(websocket)
+    
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+    
+    async def broadcast(self, message: str):
+        for connection in self.activate_connections:
+            await connection.send_text(message)
 
-# class Item(BaseModel):
-#     name: str
-#     price: float
-#     is_offer: Union[bool, None] = None
+manager = ConnectionManager()
 
-@app.get("/")
-def read_main():
-    return {"msg" : "Hello World"}
+
+# @app.get("/")
+# async def get():
+#     return HTMLResponse(html)
+
+# websocket 생성
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    # 소켓 연결
+    await manager.connect(websocket)
+    try:
+        while True:
+            # 데이터 받고 보내기
+            data = await websocket.receive_text()
+            # await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} : {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
 
 app.include_router(question_router.router)
 app.include_router(user_router.router)
 app.include_router(answer_router.router)
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int,
-#               q: Union[str, None] = None, short: bool = False):
-#     item = {"item_id": item_id, "short":short}
-#     return {"item_id" : item_id, "q": q, "sort":short}
-
-# @app.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#     return {"item_name" : item.name, "item_id": item_id}
-
-# @app.get("/models/{model_name}")
-# async def get_model(model_name: ModelName):
-#     # 비교
-#     if model_name is ModelName.alexnet:
-#         return {"model_name": model_name, "message": "Deep Learning FTW!"}
-#     # 값 확인
-#     if model_name.value == "lenet":
-#         return {"model_name": model_name, "message": "LeCNN all the images"}
-    
-#     return {"model_name": model_name, "message": "Have some residuals"}
-
-# @app.get("/items/")
-# async def read_tem(q: str | None = Query(max_length=4)):
-#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-#     return results
-
-
-# # 순차적으로 확인하고 동작하기 때문에 /user/me를 먼저 선언해야함!
-# # 반대로 선언하게 된다면 매개변수 user_id의 값을 "me"로 하여 연결할 수 있기 때문
-# @app.get("/users/me")
-# async def read_user_me():
-#     return {"user_id": "The current user"}
-
-# @app.get("/users/{user_id}")
-# async def read_user(user_id: str):
-#     return {"user_id": user_id}
-
-# # @app.get("/users/{gender}")
-# # def get_user(gender: str):
-# #     return "Type is String"
-
-# # @app.get("/users/{num}")
-# # def get_user(num: int):
-# #     return "Type is int"
--- Fast API/Fast_api_demo/frontend/src/routes/Home.svelte
+++ Fast API/Fast_api_demo/frontend/src/routes/Home.svelte
@@ -9,8 +9,8 @@
       question_list = json
     })
   }
-
   get_question_list()
+
 </script>
 
 <div class="container my-3">
@@ -38,5 +38,40 @@
     </tbody>
   </table>
   <a use:link href="/question-create" class="btn btn-primary">질문 등록</a>
-  
+
+  <br>
+  <br>
+
+  <!-- 채팅 -->
+  <body>
+    <div class="container">
+      <h2 class="my-3">Websocket Chat</h2>
+      <h3>Your ID: <span id="ws-id"></span></h3>
+      <form action="" onsubmit="sendMessage(event)">
+        <div class="input-group mb-3">
+          <input type="text" id="messageText" autocomplete="off" placeholder="Message">
+          <button class="btn btn-primary" type="submit" id="button-addon2">Send</button>
+        </div>
+      </form>
+      <ul id="messages"></ul>
+    </div>
+    <script>
+      let client_id = Date.now() % 10000
+      document.querySelector("#ws-id").textContent = client_id;
+      let ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+      ws.onmessage = function(event) {
+          let messages = document.getElementById('messages')
+          let message = document.createElement('li')
+          let content = document.createTextNode(event.data)
+          message.appendChild(content)
+          messages.appendChild(message)
+      }
+      function sendMessage(event) {
+          let input = document.getElementById("messageText")
+          ws.send(input.value)
+          input.value = ''
+          event.preventDefault()
+      }
+    </script>
+  </body>
 </div>
